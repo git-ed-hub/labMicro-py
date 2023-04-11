16a66f1ddbb0d08a73941298d7c6182ec17f736d
git-ed-hub <eduardo.annk@gmail.com>
2023-04-10 21:18:34 -0600

    Actualizacion del mensaje de Bienvenida

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,8 @@
-print("***Hello World***")
+#print("***Hello World***")
+
+def welcome():
+    print("***   Hello World   ***")
+    print("***   Hello World   ***")
+    print("***   Hello World   ***")
+
+welcome()
